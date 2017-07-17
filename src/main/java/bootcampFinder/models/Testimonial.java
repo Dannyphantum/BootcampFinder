54c1b4d11df043d743b57f97ebec9fcb9cb62a7f
glemma32 <genet.woldeyes@gmail.com>
2017-07-17 17:36:05 -0400

    all models added

--- src/main/java/bootcampFinder/models/Testimonial.java
+++ src/main/java/bootcampFinder/models/Testimonial.java
@@ -1,7 +1,41 @@
 package bootcampFinder.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by student on 7/17/17.
  */
 public class Testimonial {
+
+    private long userId;
+
+    private String message;
+
+    private long bootcampId;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getBootcampId() {
+        return bootcampId;
+    }
+
+    public void setBootcampId(long bootcampId) {
+        this.bootcampId = bootcampId;
+    }
 }
