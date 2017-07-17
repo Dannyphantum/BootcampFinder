62d8043a933c490c20a29d41cdec504f2fb3f2c1
Orion Wolf_Hubbard <owolfhu1@gmail.com>
2017-07-17 19:36:17 -0400

    starting spring app

--- src/main/java/bootcampFinder/controller/HomeController.java
+++ src/main/java/bootcampFinder/controller/HomeController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    @RequestMapping("/")
+    public String index() {
+        return "login";
+    }
+
     @RequestMapping("/login")
     public String login() {
         return "login";
